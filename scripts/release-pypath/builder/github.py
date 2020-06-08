79afed555a371dcbb5e9f58397560d5d7f84c65b
Jacob Beck <jake@fishtownanalytics.com>
2020-06-08 14:46:14 -0600

    initial work on getting a github release out on commits

---
+++ scripts/release-pypath/builder/github.py
@@ -0,0 +1,48 @@
+import json
+import os
+from urllib.request import urlopen, Request
+
+from .common import ReleaseFile, EnvironmentInfo
+
+
+def make_post(release: ReleaseFile):
+    GITHUB_API_TOKEN = os.getenv('DBT_GITHUB_API_TOKEN')
+    data = {
+        'tag_name': f'v{release.version}',
+        'target_commitish': release.branch,
+        'name': f'dbt {release.version}',
+        'body': release.notes,
+    }
+    request = Request(
+        url='https://api.github.com/repos/fishtown-analytics/dbt/releases',
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'token {GITHUB_API_TOKEN}',
+        },
+        data=json.dumps(data),
+    )
+    print(f'Creating release with data:\n{data}')
+    try:
+        with urlopen(request) as fp:
+            resp_data = fp.read()
+    except Exception as exc:
+        raise ValueError(
+            f'Could not create release {release.version}: {exc}'
+        ) from exc
+    print(f'Github response:\n{resp_data}')
+
+
+def make_github_release(args=None):
+    env = EnvironmentInfo()
+    release = ReleaseFile.from_artifacts(env)
+    make_post(release)
+
+
+def add_github_parsers(subparsers):
+    github_sub = subparsers.add_parser(
+        'github',
+        help='Create the github release'
+    )
+    github_subs = github_sub.add_subparsers(title='Available sub-commands')
+    create_release = github_subs.add_parser('create-release')
+    create_release.set_defaults(func=make_github_release)
