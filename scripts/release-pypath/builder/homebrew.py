63527b7ce94b1fca00d4e676eb717195d02aabd0
Jacob Beck <jake@fishtownanalytics.com>
2020-07-10 11:51:14 -0600

    pin to python 3.8

--- scripts/release-pypath/builder/homebrew.py
+++ scripts/release-pypath/builder/homebrew.py
@@ -122,7 +122,7 @@ class HomebrewTemplate:
               depends_on "libffi"
               depends_on "openssl@1.1"
               depends_on "postgresql"
-              depends_on "python"
+              depends_on "python@3.8"
 
             {dependencies}
               def install
